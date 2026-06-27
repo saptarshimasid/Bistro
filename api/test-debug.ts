14667111711f71d53b71c04d4c79fbb892148ace
Saptarshi Masid <saptarshimasid@Saptarshis-MacBook-Air.local>
2026-06-27 18:33:13 +0530

    deploy: update diagnostic test to import db.ts

--- api/test-debug.ts
+++ api/test-debug.ts
@@ -1,8 +1,9 @@
 import { GoogleGenAI } from "@google/genai";
 import { Pool } from "pg";
 import express from "express";
+import { initDb } from "../db";
 
 export default function handler(req: any, res: any) {
   res.setHeader("Content-Type", "text/plain");
-  res.status(200).send("Diagnostic test: All main imports loaded successfully!");
+  res.status(200).send("Diagnostic test: Import of db.ts succeeded!");
 }
