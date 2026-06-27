a321bcc4ff89791bcd8af48f1619f5b526a8ccea
Saptarshi Masid <saptarshimasid@Saptarshis-MacBook-Air.local>
2026-06-27 19:22:44 +0530

    deploy: update test-debug to minimal TypeScript handler

--- api/test-debug.ts
+++ api/test-debug.ts
@@ -1,12 +1,4 @@
-import { loadAllData } from "../db";
-
 export default async function handler(req: any, res: any) {
   res.setHeader("Content-Type", "text/plain");
-  try {
-    const data = await loadAllData();
-    res.setHeader("Content-Type", "application/json");
-    res.status(200).send(JSON.stringify(data, null, 2));
-  } catch (error: any) {
-    res.status(500).send(`Crash: ${error.message}\n${error.stack}`);
-  }
+  res.status(200).send("Hello from minimal TypeScript handler!");
 }
