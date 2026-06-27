d576a8cc537cca1d7ddad04562ac87dee454520e
Saptarshi Masid <saptarshimasid@Saptarshis-MacBook-Air.local>
2026-06-27 18:30:25 +0530

    deploy: add test-debug endpoint to isolate dependency load issues

---
+++ api/test-debug.ts
@@ -0,0 +1,8 @@
+import { GoogleGenAI } from "@google/genai";
+import { Pool } from "pg";
+import express from "express";
+
+export default function handler(req: any, res: any) {
+  res.setHeader("Content-Type", "text/plain");
+  res.status(200).send("Diagnostic test: All main imports loaded successfully!");
+}
