54de3acdc9bbc9903c2340cafb9b9b4f9f72ac54
Saptarshi Masid <saptarshimasid@Saptarshis-MacBook-Air.local>
2026-06-27 19:18:02 +0530

    deploy: update diagnostic test to import mockData directly

--- api/test-debug.ts
+++ api/test-debug.ts
@@ -1,10 +1,9 @@
-import { testDummy } from "../db";
+import { INITIAL_MENU_ITEMS } from "../src/data/mockData";
 
 export default async function handler(req: any, res: any) {
   res.setHeader("Content-Type", "text/plain");
   try {
-    const msg = testDummy();
-    res.status(200).send(`Success calling testDummy: ${msg}`);
+    res.status(200).send(`Success: ${INITIAL_MENU_ITEMS.length} items`);
   } catch (error: any) {
     res.status(500).send(`Crash: ${error.message}\n${error.stack}`);
   }
