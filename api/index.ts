5cbe53065d69e3f8dffa9e4593693b64069746f9
Saptarshi Masid <saptarshimasid@Saptarshis-MacBook-Air.local>
2026-06-27 18:50:50 +0530

    deploy: wrap Express app in a Promise resolving on res.end in api/index.ts

--- api/index.ts
+++ api/index.ts
@@ -1,5 +1,19 @@
 import app from '../server';
 
 export default function handler(req: any, res: any) {
-  return app(req, res);
+  return new Promise<void>((resolve, reject) => {
+    // Intercept response finish
+    const originalEnd = res.end;
+    res.end = function (...args: any[]) {
+      originalEnd.apply(res, args);
+      resolve();
+    };
+    
+    // Delegate to Express
+    try {
+      app(req, res);
+    } catch (err) {
+      reject(err);
+    }
+  });
 }
