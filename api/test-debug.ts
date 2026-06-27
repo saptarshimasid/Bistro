c3d0b80e89947d2df305f0747a9c04305e4c2455
Saptarshi Masid <saptarshimasid@Saptarshis-MacBook-Air.local>
2026-06-27 19:54:34 +0530

    deploy: update test-debug to minimal handler after type:module removal

--- api/test-debug.ts
+++ api/test-debug.ts
@@ -1,12 +1,4 @@
-import { loadAllData } from "../db";
-
 export default async function handler(req: any, res: any) {
-  res.setHeader("Content-Type", "application/json");
-  try {
-    const data = await loadAllData();
-    res.status(200).send(JSON.stringify(data, null, 2));
-  } catch (error: any) {
-    res.setHeader("Content-Type", "text/plain");
-    res.status(500).send(`Crash: ${error.message}\n${error.stack}`);
-  }
+  res.setHeader("Content-Type", "text/plain");
+  res.status(200).send("Hello from minimal TypeScript handler after type:module removal!");
 }
